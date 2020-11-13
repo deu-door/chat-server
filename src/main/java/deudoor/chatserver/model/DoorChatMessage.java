364caced6a357f7f228846e69236abdcc4680992
solo5star <solo_5@naver.com>
2020-11-14 02:04:49 +0900

    DoorChatMessage: Apply Entity annotation and add id field

--- src/main/java/deudoor/chatserver/model/DoorChatMessage.java
+++ src/main/java/deudoor/chatserver/model/DoorChatMessage.java
@@ -1,31 +1,42 @@
 package deudoor.chatserver.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DoorChatMessage implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String message;
+
     // i.e. 20172014
     private String userId;
+
     // i.e. 홍길동
     private String user;
+
     private Long timestamp;
 
     private String topic = "/topic/public";
-    private Long roomId;
 
     private String fileName;
     private String rawData;
-
-    public DoorChatMessage() {
-
-    }
 
     public DoorChatMessage(String message, String user) {
         this.user = user;
@@ -43,6 +54,6 @@ public class DoorChatMessage implements Serializable {
 
     @Override
     public String toString() {
-        return "DoorChatMessage [user=" + this.user + ", message=" + this.message + "]";
+        return "DoorChatMessage [userId=" + userId + "user=" + user + ", message=" + message + "]";
     }
 }
