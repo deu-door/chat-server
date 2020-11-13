8c79aa78c56c40d74c9214c8eedc9e204bb557b0
solo5star <solo_5@naver.com>
2020-11-14 02:07:42 +0900

    DoorChatController: Add chat history mapping (/history?topic=)

--- src/main/java/deudoor/chatserver/controller/DoorChatController.java
+++ src/main/java/deudoor/chatserver/controller/DoorChatController.java
@@ -1,5 +1,6 @@
 package deudoor.chatserver.controller;
 
+import deudoor.chatserver.repository.DoorChatMessageRepository;
 import deudoor.chatserver.service.KafkaReceiver;
 import deudoor.chatserver.model.DoorChatMessage;
 import deudoor.chatserver.model.DoorChatRoom;
@@ -10,10 +11,9 @@ import deudoor.chatserver.service.KafkaSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,10 +33,20 @@ public class DoorChatController {
     @Autowired
     private DoorChatRoomUserRepository doorChatRoomUserRepository;
 
+    @Autowired
+    private DoorChatMessageRepository doorChatMessageRepository;
+
     // Client --(/chat)--> Spring Boot --> Kafka
     @MessageMapping("/message")
     public void sendMessage(DoorChatMessage message) {
         sender.send(message);
     }
-    
+
+    @GetMapping("/history")
+    public List<DoorChatMessage> getHistories(
+            @RequestParam @NotBlank String topic
+    ) {
+        return doorChatMessageRepository.findByTopic(topic);
+    }
+
 }
